94bf2683760ba9612b4d443ba06a7e451a0dc421
Faraz <mohmdfarazali@gmail.com>
2023-08-26 14:34:41 +0530

    updated projects and skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,15 +4,31 @@ import HTML from "../assets/html.png";
 import CSS from "../assets/css.png";
 import JavaScript from "../assets/javascript.png";
 import ReactImg from "../assets/react.png";
-import Node from "../assets/node.png";
+// import Node from "../assets/node.png";
 import Cpp from "../assets/cpp.png";
 import Tailwind from "../assets/tailwind.png";
 import Mongo from "../assets/mongo.png";
+import Nextjs from "../assets/nextjs.png";
+import Firebase from "../assets/firebase.png";
+import Github from "../assets/github.png";
+
+const skillsData = [
+  { name: "C++", src: Cpp, color: "border-blue-700", alt: "cpp" },
+  { name: "HTML", src: HTML, color: "border-orange-400", alt: "html" },
+  { name: "CSS", src: CSS, color: "border-blue-400", alt: "css" },
+  { name: "JavaScript", src: JavaScript, color: "border-yellow-400", alt: "js" },
+  { name: "ReactJS", src: ReactImg, color: "border-cyan-400", alt: "react" },
+  { name: "Next.js", src: Nextjs, color: "border-white", alt: "nextjs" },
+  { name: "Tailwind CSS", src: Tailwind, color: "border-cyan-600", alt: "tailwind" },
+  // { name: "NodeJS", src: Node, color: "border-green-500", alt: "node" },
+  { name: "MongoDB", src: Mongo, color: "border-green-400", alt: "mongo" },
+  { name: "Firebase", src: Firebase, color: "border-yellow-600", alt: "firebase" },
+  { name: "Git/GitHub", src: Github, color: "border-cyan-600", alt: "github" },
+];
 
 const Skills = () => {
   return (
     <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
-      {/* container  */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
@@ -20,46 +36,16 @@ const Skills = () => {
           </p>
           <p className="py-4">\\ These are the technologies I work on</p>
         </div>
-        {/* container for all the skills */}
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          {/* particular container for image and text
-                <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={HTML} alt="html" />
-            <p className="py-4">HTML</p>
-          </div>
-            */}
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-md border border-blue-700">
-            <img className="w-20 mx-auto pt-2" src={Cpp} alt="cpp" />
-            <p className="py-4">C++</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-md border border-orange-400">
-            <img className="w-20 mx-auto pt-2" src={HTML} alt="html" />
-            <p className="py-4">HTML</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-md border border-blue-400">
-            <img className="w-20 mx-auto pt-2" src={CSS} alt="html" />
-            <p className="py-4">CSS</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-md border border-yellow-400">
-            <img className="w-20 mx-auto pt-2" src={JavaScript} alt="html" />
-            <p className="py-4">JS</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-md border border-cyan-400">
-            <img className="w-20 mx-auto pt-2" src={ReactImg} alt="html" />
-            <p className="py-4">ReactJS</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-md border border-green-500">
-            <img className="w-20 mx-auto pt-2" src={Node} alt="html" />
-            <p className="py-4">NodeJS</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-md border border-green-400">
-            <img className="w-20 mx-auto pt-2" src={Mongo} alt="html" />
-            <p className="py-4">MongoDB</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-md border border-cyan-600">
-            <img className="w-20 mx-auto pt-2" src={Tailwind} alt="html" />
-            <p className="py-4">Tailwind CSS</p>
-          </div>
+          {skillsData.map((skill, index) => (
+            <div
+              key={index}
+              className={`shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-md border ${skill.color}`}
+            >
+              <img className="w-20 mx-auto pt-2" src={skill.src} alt={skill.alt} />
+              <p className="py-4">{skill.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,6 +3,41 @@ import ThreeDCal from "../assets/projects/3dcal.jpg";
 import Gify from "../assets/projects/gify.jpg";
 import SwiftChat from "../assets/projects/SwiftChat.png";
 import TaskSavvy from "../assets/projects/TaskSavvy.png";
+import Threadly from "../assets/projects/Threadly.png";
+
+const projects = [
+  {
+    name: "Threadly 🪡",
+    image: Threadly,
+    liveLink: "https://threadly-nine.vercel.app/",
+    codeLink: "https://github.com/mohmdfarazali/threadly",
+  },
+  {
+    name: "Swift Chat 💬",
+    image: SwiftChat,
+    liveLink: "https://swift-chat-inky.vercel.app/login",
+    codeLink: "https://github.com/mohmdfarazali/swift-chat",
+  },
+  {
+    name: "Task Savvy 📝",
+    image: TaskSavvy,
+    liveLink: "https://todo-nextjs-firebase-two.vercel.app/",
+    codeLink: "https://github.com/mohmdfarazali/todo-nextjs-firebase",
+  },
+  {
+    name: "Gify - Generate Gifs ",
+    image: Gify,
+    liveLink: "https://mzbu95.csb.app/",
+    codeLink: "https://github.com/mohmdfarazali/Gify",
+  },
+  {
+    name: "3D Calculator",
+    image: ThreeDCal,
+    liveLink: "https://mohmdfarazali.github.io/3D-Calculator/",
+    codeLink: "https://github.com/mohmdfarazali/3D-Calculator",
+  },
+  // Add more projects here if needed
+];
 
 const Work = () => {
   return (
@@ -15,107 +50,32 @@ const Work = () => {
           <p className="py-6">\\ Check out some of my recent work</p>
         </div>
 
-        {/* grid/card container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {/* grid item */}
-          {/* --------------------- */}
-          <div
-            style={{ backgroundImage: `url(${SwiftChat})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* hover effects */}
-            <div className="opacity-0 group-hover:opacity-100 z-10">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                SwiftChat
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://swift-chat-inky.vercel.app/login">
-                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Live
-                  </button>
-                </a>
-                <a href="https://github.com/mohmdfarazali/swift-chat">
-                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+          {projects.map((project, index) => (
+            <div
+              key={index}
+              style={{ backgroundImage: `url(${project.image})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              <div className="opacity-0 group-hover:opacity-100 z-10">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  {project.name}
+                </span>
+                <div className="pt-8 text-center">
+                  <a href={project.liveLink}>
+                    <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Live
+                    </button>
+                  </a>
+                  <a href={project.codeLink}>
+                    <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          {/* --------------------- */}
-          {/* --------------------- */}
-          <div
-            style={{ backgroundImage: `url(${TaskSavvy})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* hover effects */}
-            <div className="opacity-0 group-hover:opacity-100 z-10">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Task Savvy
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://todo-nextjs-firebase-two.vercel.app/">
-                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Live
-                  </button>
-                </a>
-                <a href="https://github.com/mohmdfarazali/todo-nextjs-firebase">
-                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* --------------------- */}
-          <div
-            style={{ backgroundImage: `url(${Gify})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* hover effects */}
-            <div className="opacity-0 group-hover:opacity-100 z-10">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Gify - Generate Gifs
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://mzbu95.csb.app/">
-                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Live
-                  </button>
-                </a>
-                <a href="https://github.com/mohmdfarazali/Gify">
-                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* --------------------- */}
-          <div
-            style={{ backgroundImage: `url(${ThreeDCal})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* hover effects */}
-            <div className="opacity-0 group-hover:opacity-100 z-10">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                3D Calculator
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://mohmdfarazali.github.io/3D-Calculator/">
-                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Live
-                  </button>
-                </a>
-                <a href="https://github.com/mohmdfarazali/3D-Calculator">
-                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* --------------------- */}
+          ))}
         </div>
       </div>
     </div>
